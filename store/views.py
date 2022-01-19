3e80854b4944fc2514e177d04bee0412ecbd341a
Abdulgoniy Farkhodov <user@kali.kali>
2022-01-20 04:58:17 +0500

    add_to_cart func

--- store/views.py
+++ store/views.py
@@ -41,10 +41,12 @@ def store(request,category_slug=None):
 
 def product_detail(request,category_slug,product_slug):
     try:
-        single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        single_product = get_object_or_404(Product,category__slug=category_slug,slug=product_slug)
     except Exception as e:
         raise e
     context = {
         'single_product':single_product,
     }
     return render(request,'store/product_detail.html',context)
+
+
